2b6277ef6f121f95784c2af5c32d289906a20fa4
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2025-01-07 22:06:22 +0530

    feat: create APIs

---
+++ src/services/studentService.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { Student } from "../types/Student";
+
+const API_URL = "http://localhost:8080/api/student";
+
+export const getAllStudents = async () => {
+     const response = await axios.post(`${API_URL}/get`);
+     return response.data.data;
+};
+
+export const getStudentById = async (id: string) => {
+     const response = await axios.get(`${API_URL}/${id}`);
+     return response.data.data;
+};
+
+export const createStudent = async (student: Student) => {
+     const response = await axios.post(`${API_URL}/save`, student);
+     return response.data.data;
+};
+
+export const updateStudent = async (id: string, student: Student) => {
+     const response = await axios.post(`${API_URL}/update/${id}`, student);
+     return response.data.data;
+};
+
+export const deleteStudent = async (id: string) => {
+     const response = await axios.post(`${API_URL}/delete/${id}`);
+     return response.data.message;
+};
